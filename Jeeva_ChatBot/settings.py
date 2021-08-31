829b6e2ad049c8d08f653b37c164cee942220de3
Anurag Nadda <anuragn@squadmindsinc.com>
2021-08-31 16:02:09 +0530

    print template dirs

--- Jeeva_ChatBot/settings.py
+++ Jeeva_ChatBot/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = (BASE_DIR/ "templates")
-
+print(TEMPLATE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
